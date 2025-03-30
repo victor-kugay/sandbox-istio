0d1f3449dc107a2a35c1d299f7d5d1b379258c3d
v.kugay <v.kugay@tinkoff.ru>
2025-03-30 22:50:25 +0300

    Развертывание машрутизации трафика с помощью Istio

--- build/index.js
+++ build/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('*', function (req, res) {
-    res.send({status: 'ok'});
+  res.send({status: 'ok'});
 });
 
 app.get('/status', function (req, res) {
-    res.send({status: 'ok'});
+  res.send({status: 'ok'});
 });
 
 app.listen(3000);
